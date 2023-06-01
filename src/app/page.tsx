997ffadcd7fe111fcf47d1acf0f6d15eba817b91
Huy Nguyen <huyn.nguyen95@gmail.com>
2023-06-01 10:38:00 -0500

    refactor(app/page.tsx): take TimelineItem.tsx out of Timeline.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import { Timeline } from '@components/Timeline';
+import { Timeline } from "@components/Timeline";
+import { TimelineItem } from "@components/TimelineItem";
 
 export default function Homepage() {
   return (
     <div>
       <Timeline />
+      <TimelineItem />
     </div>
   );
 }
---
+++ src/components/TimelineItem.tsx
@@ -0,0 +1,13 @@
+export const TimelineItem = () => {
+  return (
+    <div className="px-4 py-5 sm:p-6">
+      <div className="overflow-hidden rounded-md bg-white shadow">
+        <ul role="list" className="divide-y divide-gray-200">
+          <li className="relative flex cursor-pointer justify-between gap-x-6 px-4 py-5 hover:bg-gray-50 sm:px-6 lg:px-8">
+            Timeline Item
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
