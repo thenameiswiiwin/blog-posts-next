fac9194985f3e31a2e239a4403701710a9ab23ac
Huy Nguyen <huyn.nguyen95@gmail.com>
2023-06-01 10:56:33 -0500

    feat(Timeline.tsx): keep track of state of which period is active

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { Timeline } from "@components/Timeline";
-import { TimelineItem } from "@components/TimelineItem";
+import { Timeline } from '@components/Timeline';
+import { TimelineItem } from '@components/TimelineItem';
 
 export default function Homepage() {
   return (
--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,9 +1,15 @@
-import clsx from "clsx";
-import Link from "next/link";
+'use client';
 
-const periods = ["Today", "This Week", "This Month"];
+import clsx from 'clsx';
+import Link from 'next/link';
+import { useState } from 'react';
+
+const periods = ['Today', 'This Week', 'This Month'];
 
 export const Timeline = () => {
+  const [selectedPeriod, setSelectedPeriod] = useState('Today');
+
+  const selectPeriod = (period: string) => setSelectedPeriod(period);
   return (
     <div className="rounded-lg px-4 py-5 shadow-lg sm:px-6 sm:py-2">
       <div>
@@ -27,13 +33,14 @@ export const Timeline = () => {
               {periods.map((period) => (
                 <Link
                   key={period}
-                  href={""}
-                  aria-current={period ? "page" : undefined}
+                  href="#"
+                  aria-current={period ? 'page' : undefined}
+                  onClick={() => selectPeriod(period)}
                   className={clsx(
-                    "w-1/4 cursor-pointer border-b-2 px-1 py-4 text-center text-sm font-medium",
-                    period
-                      ? "border-green-500 text-green-600"
-                      : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                    'w-1/4 cursor-pointer border-b-2 px-1 py-4 text-center text-sm font-medium',
+                    selectedPeriod === period
+                      ? 'border-green-500 text-green-600'
+                      : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
                   )}
                 >
                   {period}
