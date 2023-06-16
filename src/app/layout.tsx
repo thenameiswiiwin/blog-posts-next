d36bb68e14fc708077ef633b9da7d9f1ddaec56d
Huy Nguyen <huyn.nguyen95@gmail.com>
2023-06-15 21:28:16 -0500

    fix(app/layout.tsx): fixed Next warning: extra attributes from server

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,14 +9,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <meta
           name="viewport"
           content="width=device-width, height=device-height, initial-scale=1"
         />
       </head>
-      <body>
+      <body suppressHydrationWarning>
         <div className="overflow-hidden rounded-lg bg-white">
           <div className="container mx-auto px-4 sm:px-6 lg:px-8">
             <Navbar />
