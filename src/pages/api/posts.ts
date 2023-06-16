93110b51f785964a90d307698f50f65cb9d69ca0
Huy Nguyen <huyn.nguyen95@gmail.com>
2023-06-16 11:12:47 -0500

    feat(api/posts.ts): post endpoint

--- src/pages/api/posts.ts
+++ src/pages/api/posts.ts
@@ -1,8 +1,16 @@
-import { thisMonth, thisWeek, today } from '@lib/posts';
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { thisMonth, thisWeek, today } from "@lib/posts";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 const allPosts = [today, thisWeek, thisMonth];
 
 export default async function posts(req: NextApiRequest, res: NextApiResponse) {
-  res.json(allPosts);
+  if (req.method === "GET") {
+    res.json(allPosts);
+  }
+
+  if (req.method === "POST") {
+    const post = { ...req.body, id: (Math.random() * 100000).toFixed() };
+    allPosts.push(post);
+    res.json(post);
+  }
 }
